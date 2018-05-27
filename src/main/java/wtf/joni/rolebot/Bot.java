a4e466c899f902e8493192326b666a8267b65aaa
Joni Mettälä <joni.mettala@gmail.com>
2018-05-27 12:31:17 +0300

    Move user configurable stuff to external json file
    Makes updating Bot.java easier when your secret token is not in it.
    Also works as a great place to store other user preferences.

--- src/main/java/wtf/joni/rolebot/Bot.java
+++ src/main/java/wtf/joni/rolebot/Bot.java
@@ -2,8 +2,14 @@ package wtf.joni.rolebot;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
 
 /**
  * Main class of the bot
@@ -11,17 +17,32 @@ import javax.security.auth.login.LoginException;
 public class Bot {
 
     /**
-     * Character which starts all the bot commands
-     * For example, if "!", commands look like: !ping
+     * Secret bot token, generated from Discord developer pages
+     * Imported from settings.json
      */
-    public static final String CMD = ".";
+    private static String SECRET_TOKEN;
 
     /**
-     * Secret bot token, generated from Discord developer pages
+     * Character which starts all the bot commands
+     * For example, if "!", commands look like: !ping
+     * Imported from settings.json
      */
-    private static final String SECRET_TOKEN = "";
+    public static String CMD;
 
     public static void main(String[] args) {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            URL path = Bot.class.getResource("settings.json");
+            File f = new File(path.getFile());
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new BufferedReader(new FileReader(f)));
+
+            SECRET_TOKEN = (String) jsonObject.get("Token");
+            CMD = (String) jsonObject.get("CommandKey");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         try {
             new JDABuilder(AccountType.BOT)
                     .setToken(SECRET_TOKEN)
