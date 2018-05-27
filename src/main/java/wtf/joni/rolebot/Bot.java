f2ce18cece106058a5aaa83afb382bb51b848932
Joni Mettälä <joni.mettala@gmail.com>
2018-05-27 13:49:11 +0300

    Set bot to watch cute cat videos while in DND
    Because online without a status was boring

--- src/main/java/wtf/joni/rolebot/Bot.java
+++ src/main/java/wtf/joni/rolebot/Bot.java
@@ -2,6 +2,8 @@ package wtf.joni.rolebot;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -46,6 +48,8 @@ public class Bot {
         try {
             new JDABuilder(AccountType.BOT)
                     .setToken(SECRET_TOKEN)
+                    .setGame(Game.watching("cute cat videos"))
+                    .setStatus(OnlineStatus.DO_NOT_DISTURB)
                     .addEventListener(new PingListener())
                     .buildBlocking();
         } catch (LoginException | InterruptedException e) {
